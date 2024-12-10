4e97381633604cacc075a3814cbcbd40e1643473
AbhilashPoshanagari <63783012+AbhilashPoshanagari@users.noreply.github.com>
2024-12-10 19:17:14 +0000

    test cli

--- wikibot.py
+++ wikibot.py
@@ -1,6 +1,12 @@
-import wikipedia
+import click
+from mylib.bot import scrape
 
-def scrape(name='Microsoft', length=1):
-    result = wikipedia.summary(name, sentences=length)
-    return result
-print(scrape('facebook'))
+@click.command()
+@click.option('--length', help='A person to greet')
+@click.option('--name', help='Web page ')
+def cli(name, length):
+    result = scrape(name, length)
+    click.echo(click.style(f'{result}', fg='blue'))
+
+if __name__ == '__main__':
+    cli()
